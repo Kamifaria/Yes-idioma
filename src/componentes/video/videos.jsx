23b8c9c04aff368981bc739e839af531c7410f06
taticoimbra89 <tati.coimbra@hotmail.com>
2024-10-04 18:44:05 -0300

    continuação das alterações

--- src/componentes/video/videos.jsx
+++ src/componentes/video/videos.jsx
@@ -4,7 +4,7 @@ const VideoComponent = () => {
 
     return(
         <video width="600" height="400" controls autoPlay loop
-        src="../Banner/Institucional Vilarejo Praia.mp4" type="video/mp4">
+        src="../Banner/" type="video/mp4">
             videos teste
         </video>
     );
